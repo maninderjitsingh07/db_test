0c569a5cddee464841ff26050342c8b015b2ca74
maninderjitsingh07 <148739301+maninderjitsingh07@users.noreply.github.com>
2024-05-18 20:21:21 -0400

    Update load_cricbuzz_rank_data_GC.py

--- Airflow/cric_icc_stats/load_cricbuzz_rank_data_GC.py
+++ Airflow/cric_icc_stats/load_cricbuzz_rank_data_GC.py
@@ -1,12 +1,16 @@
 import requests
 import csv
+import json
 import os
 from google.cloud import storage
 import pandas as pd
+from airflow import DAG
 from airflow.providers.google.cloud.operators.gcs import GCSCreateBucketOperator
+from airflow.contrib.operators.gcs_to_bq import GoogleCloudStorageToBigQueryOperator
+from airflow.operators.python_operator import PythonOperator
 
 default_args = {
-    'owner': 'Amara',
+    'owner': 'manindejit',
     'depends_on_past': False,
     'email_on_failure': False,
     'email_on_retry': False,
@@ -24,6 +28,8 @@ headers = {
 }
 field_names = ['rank', 'name', 'country','rating','points','lastUpdatedOn']
 bucket_name = 'raw_bucket_cric_icc_ranking_data'
+FILE_NAME="cricbuzz_rank_info.csv"
+STAGING_DATASET="cric_info"
 
 
 # Get project information 
@@ -43,7 +49,7 @@ def get_data():
         storage_client = storage.Client()  
         bucket=storage_client.bucket(bucket_name)
         # Upload the data to the selected bucket
-        blob = bucket.blob('cricbuzz_rank_info.csv')
+        blob = bucket.blob(FILE_NAME)
         blob.upload_from_string(df)
         print(f"data sucessfully uploadesd to {bucket_name}")
 
@@ -54,18 +60,41 @@ with DAG('cric_rank_info',
          default_args=default_args, 
          tags=["gcs", "bq"]
 ) as dag:
+
         create_bucket = GCSCreateBucketOperator(
             task_id="create_bucket",
             bucket_name=bucket_name,
-            project_id=get_project_info,
+            project_id=get_project_info(),
         )
+
         load_data_to_bucket = PythonOperator(
-        task_id = 'load_data_to_bucket',
-        python_callable = get_data,
+             task_id = 'load_data_to_bucket',
+             python_callable = get_data,
         )
+
+        load_to_bq = GCSToBigQueryOperator(
+             task_id = 'load_to_bq',
+             bucket = bucket_name,
+             source_objects = ['stock_data.csv'],
+             destination_project_dataset_table = f'{get_project_info()}:{STAGING_DATASET}.rank',
+             write_disposition='WRITE_TRUNCATE',
+             source_format = 'csv',
+             allow_quoted_newlines = 'true',
+             skip_leading_rows = 1,
+             schema_fields=[
+                  {'name': 'rank', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+                  {'name': 'name', 'type': 'STRING', 'mode': 'NULLABLE'},
+                  {'name': 'country', 'type': 'STRING', 'mode': 'NULLABLE'},
+                  {'name': 'rating', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+                  {'name': 'points', 'type': 'INTEGER', 'mode': 'NULLABLE'},
+                  {'name': 'lastUpdatedOn', 'type': 'DATE', 'mode': 'NULLABLE'},
+        ],
+        )
+
         (
         create_bucket
         >> pull_stock_data_to_gcs
+        >> load_to_bq
         )
 
         
